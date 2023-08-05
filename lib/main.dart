dd79d6873d90b6b72d9cc779787810e827a7489e
Obumuneme Nwabude <obumnwabude@gmail.com>
2023-08-05 11:35:01 +0100

    chore: improve infos, their units, and obtaining

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:battery_info/battery_info_plugin.dart';
 import 'package:battery_info/model/android_battery_info.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +8,11 @@ import 'widgets/battery.dart';
 import 'widgets/info_list.dart';
 
 void main() => runApp(const MyApp());
+
+extension on AndroidBatteryInfo {
+  int get level => batteryLevel ?? 0;
+  Color get color => level > 20 ? Colors.green : Colors.red;
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -22,19 +29,23 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  int _getBatteryLevel(AndroidBatteryInfo i) => i.batteryLevel!;
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
-  Color _getBatteryColor(AndroidBatteryInfo i) {
-    if (i.batteryLevel! > 75) {
-      return Colors.green;
-    } else if (i.batteryLevel! > 20) {
-      return Colors.amber;
-    } else {
-      return Colors.red;
-    }
+class _HomeScreenState extends State<HomeScreen> {
+  AndroidBatteryInfo? _info;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(milliseconds: 100), (_) async {
+      final info = await BatteryInfoPlugin().androidBatteryInfo;
+      setState(() => _info = info);
+    });
   }
 
   @override
@@ -50,43 +61,35 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: StreamBuilder<AndroidBatteryInfo?>(
-        stream: BatteryInfoPlugin().androidBatteryInfoStream,
-        builder: (context, snapshot) {
-          return ListView(
-            padding: const EdgeInsets.symmetric(vertical: 64, horizontal: 32),
-            children: snapshot.data == null
-                ? const [
-                    Text('Please Wait ...', textAlign: TextAlign.center),
-                    SizedBox(height: 96),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          width: 128,
-                          height: 128,
-                          child: CircularProgressIndicator(strokeWidth: 10),
-                        ),
-                      ],
-                    ),
-                  ]
-                : [
-                    Text(
-                      '${_getBatteryLevel(snapshot.data!).toString()}%',
-                      style: const TextStyle(fontSize: 40),
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 16),
-                    Battery(
-                      _getBatteryColor(snapshot.data!),
-                      _getBatteryLevel(snapshot.data!),
-                    ),
-                    const SizedBox(height: 32),
-                    const Text('Infos', style: TextStyle(fontSize: 24)),
-                    InfoList(info: snapshot.data!),
-                  ],
-          );
-        },
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 64, horizontal: 32),
+        children: [
+          if (_info == null) ...const [
+            Text('Please Wait ...', textAlign: TextAlign.center),
+            SizedBox(height: 96),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 128,
+                  height: 128,
+                  child: CircularProgressIndicator(strokeWidth: 2),
+                ),
+              ],
+            ),
+            SizedBox(height: 96),
+          ] else ...[
+            Text(
+              '${_info!.level}%',
+              style: const TextStyle(fontSize: 40),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 16),
+            Battery(_info!.color, _info!.level),
+            const SizedBox(height: 64),
+            InfoList(info: _info!),
+          ],
+        ],
       ),
     );
   }
--- lib/widgets/info_list.dart
+++ lib/widgets/info_list.dart
@@ -12,43 +12,70 @@ class InfoList extends StatelessWidget {
   final AndroidBatteryInfo info;
   const InfoList({Key? key, required this.info}) : super(key: key);
 
+  Info _timeInfo(int secs) {
+    String value = '';
+    final days = secs ~/ 86400;
+    if (days > 0) value += '$days day${days > 1 ? 's' : ''} ';    
+    final hrs = (secs % 86400) ~/ 1440;
+    if (hrs > 0) value += '$hrs hour${hrs > 1 ? 's' : ''} ';
+    final mins = ((secs % 86400) % 1440) ~/ 60;
+    if (mins > 0) value += '$mins min${mins > 1 ? 's' : ''} ';
+    secs = ((secs % 86400) % 1440) % 60;
+    if (secs > 0) value += '$secs sec${secs > 1 ? 's' : ''} ';
+    return Info('Remaining Charging Time', value, '');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final t = info.chargeTimeRemaining;
+    Info? timeInfo;
+    if (t != null && t > 0) timeInfo = _timeInfo(t ~/ 1000);
+
     List<Info> infos = [
-      Info('Level', info.batteryLevel, '%'),
-      Info('Health', info.health, ''),
-      Info('Capacity', info.batteryCapacity, ''),
-      Info('Current Now', info.currentNow, ''),
-      Info('Current Average', info.currentAverage, ''),
-      Info('Voltage', info.voltage, 'V'),
-      Info('Temperature', info.temperature, '°C'),
-      Info('Technology', info.technology, ''),
       Info('Charging Status', '${info.chargingStatus}'.split('.')[1], ''),
+      Info('Current Now', (info.currentNow ?? 0) / 1000000, 'A'),
+      Info('Current Average', (info.currentAverage ?? 0) / 1000000, 'A'),
+      Info('Health', info.health, ''),
+      Info('Level', info.batteryLevel, '%'),
+      Info('Realtime Battery Capacity', (info.batteryCapacity ?? 0) / 1000,
+          'mAh'),
+      if (timeInfo != null) timeInfo,
+      Info('Technology', info.technology, ''),
+      Info('Temperature', info.temperature, '°C'),
       Info('Plugged Status', info.pluggedStatus, ''),
+      Info('Voltage', (info.voltage ?? 0) / 1000, 'V'),
     ];
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: infos
-          .map(
-            (i) => [
-              const SizedBox(height: 16),
-              Text(
-                i.label,
-                style: Theme.of(context).textTheme.bodySmall,
-              ),
-              const SizedBox(height: 4),
-              Text(
-                i.value.toString() + i.unit,
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              const SizedBox(height: 16),
-              if (i.label != 'Plugged Status')
-                const Divider(height: 1, thickness: 1),
-            ],
-          )
-          .expand((w) => w)
-          .toList(),
+    return Row(
+      children: [
+        Flexible(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: infos
+                .asMap()
+                .entries
+                .map(
+                  (entry) => [
+                    const SizedBox(height: 16),
+                    Text(
+                      entry.value.label,
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      '${entry.value.value}${entry.value.unit}',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    const SizedBox(height: 16),
+                    if (entry.key != infos.length - 1)
+                      const Divider(height: 1, thickness: 1),
+                  ],
+                )
+                .expand((w) => w)
+                .toList(),
+          ),
+        ),
+      ],
     );
   }
 }
